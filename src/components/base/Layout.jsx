06222f57113ddbe4b42d7f2dba84b5fcb018ee25
cokid1998 <cokid98@naver.com>
2024-07-27 22:35:28 +0900

    Add sidebar

--- src/components/base/Layout.jsx
+++ src/components/base/Layout.jsx
@@ -3,7 +3,7 @@ import TabBar from "@/components/base/TabBar";
 function Layout({ children }) {
   return (
     <div className="flex justify-center min-h-svh relative">
-      <div className="max-w-[400px] min-w-[430px]  border-x border-[#F3F3F3]">
+      <div className="max-w-[400px] min-w-[430px] border-x border-[#F3F3F3]">
         {children}
       </div>
       <TabBar />
--- src/components/base/TabBar.jsx
+++ src/components/base/TabBar.jsx
@@ -1,25 +1,36 @@
 import { Home, User } from "lucide-react";
 import { Link } from "react-router-dom";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import Tier from "@/components/Tier/Tier";
+import caracter from "@/assets/caracter.png";
 
 function TabBar() {
   return (
-    <div className="max-w-[430px] absolute bottom-[30px] flex w-full justify-around">
-      <Link to={"/"}>
-        <div className="bg-black w-[120px] h-[50px] rounded-full pl-[10px] flex gap-[10px] items-center">
-          <div className="bg-white w-[34px] h-[34px] rounded-full flex justify-center items-center">
-            <Home />
-          </div>
-          <div className="text-white">Home</div>
-        </div>
-      </Link>
+    <>
+      <Sheet>
+        <SheetContent className="w-[300px] sm:w-[540px] flex flex-col items-center justify-center">
+          <Tier />
+          <img src={caracter} className="w-[258px] h-[444px]" />
+        </SheetContent>
+        <div className="max-w-[430px] absolute bottom-[30px] flex w-full justify-around">
+          <Link to={"/"}>
+            <div className="bg-black w-[120px] h-[50px] rounded-full pl-[10px] flex gap-[10px] items-center">
+              <div className="bg-white w-[34px] h-[34px] rounded-full flex justify-center items-center">
+                <Home />
+              </div>
+              <div className="text-white">Home</div>
+            </div>
+          </Link>
 
-      <div className="bg-black w-[120px] h-[50px] pl-[10px] gap-[5px] rounded-full flex items-center">
-        <div className="bg-white w-[34px] h-[34px] rounded-full flex justify-center items-center">
-          <User />
+          <SheetTrigger className="bg-black w-[120px] h-[50px] pl-[10px] gap-[5px] rounded-full flex items-center">
+            <div className="bg-white w-[34px] h-[34px] rounded-full flex justify-center items-center">
+              <User />
+            </div>
+            <div className="text-white">My Page</div>
+          </SheetTrigger>
         </div>
-        <div className="text-white">My Page</div>
-      </div>
-    </div>
+      </Sheet>
+    </>
   );
 }
 
